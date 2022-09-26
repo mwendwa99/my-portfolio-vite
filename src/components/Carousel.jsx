f83540f906ca2bae1e2bfa3c76ef50129fda02b3
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2022-09-26 15:41:35 +0300

    add netlify cli, styled components

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,17 +7,19 @@ import {
   CircularProgress,
   Grid,
   Box,
+  Divider,
 } from "@mui/material";
+import styled from "styled-components";
 
 export default function Example({ projects }) {
-  console.log("items", projects);
-
   return (
+    // <ScreenSize width>
     <Carousel
       // stopAutoPlayOnHover
       duration={300}
       animation="slide"
       swipe
+      fullHeightHover={false}
       // navButtonsAlwaysVisible
     >
       {typeof projects !== "undefined" ? (
@@ -37,26 +39,30 @@ export default function Example({ projects }) {
         </Paper>
       )}
     </Carousel>
+    // </ScreenSize>
   );
 }
 
 function Item(props) {
   return (
-    <Paper sx={{ height: 300, p: 2 }}>
+    <Paper sx={{ height: "100%", p: 2 }}>
       <Grid container spacing={1} sx={{ pr: 2, pl: 2 }}>
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={5}>
           <img
             style={{
               borderRadius: 5,
               boxShadow: `0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)`,
             }}
             height={270}
-            width={350}
+            width={400}
             src={props.item.image}
             alt={props.item.title}
           />
         </Grid>
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={1} sm={2}>
+          <Divider orientation="vertical" />
+        </Grid>
+        <Grid item xs={5} sm={5}>
           <Box
             sx={{
               display: "flex",
