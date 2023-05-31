bd0801a0020d2a5b8cb01f52949edba4f1102a02
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2023-05-31 12:39:14 +0300

    update pdf toolbar

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -60,7 +60,8 @@ export default function TransitionsModal() {
         <Fade in={open}>
           <Box sx={style}>
             <embed
-              src={data.url}
+              // src={data.url}
+              src={`${data.url}#toolbar=0`}
               type="text/html"
               height="100%"
               width="100%"
